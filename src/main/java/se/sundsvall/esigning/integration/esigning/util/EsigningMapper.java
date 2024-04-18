be41919b3e142c448ec3121008028bcc72c55e30
AndreSoderlund <andre.soderlund@b3.se>
2024-04-18 13:59:59 +0200

    PR-feedback-changes

--- src/main/java/se/sundsvall/esigning/integration/esigning/util/EsigningMapper.java
+++ src/main/java/se/sundsvall/esigning/integration/esigning/util/EsigningMapper.java
@@ -22,23 +22,21 @@ public final class EsigningMapper {
 		if (signingRequest == null) {
 			return null;
 		}
-
 		final var newRequest = new generated.se.sundsvall.pw_e_signing.SigningRequest();
 
-		Optional.ofNullable(signingRequest.getLanguage()).ifPresent(newRequest::setLanguage);
-		Optional.ofNullable(signingRequest.getCallbackUrl()).ifPresent(newRequest::setCallbackUrl);
-		Optional.ofNullable(signingRequest.getExpires()).ifPresent(newRequest::setExpires);
+		newRequest.setLanguage(signingRequest.getLanguage());
+		newRequest.setCallbackUrl(signingRequest.getCallbackUrl());
+		newRequest.setExpires(signingRequest.getExpires());
+		newRequest.setInitiator(toInitiator(signingRequest.getInitiator()));
+		newRequest.setNotificationMessage(toMessage(signingRequest.getNotificationMessage()));
+		newRequest.setReminder(toReminder(signingRequest.getReminder()));
+		newRequest.setSignatories(toSignatories(signingRequest.getSignatories()));
 
 		Optional.ofNullable(signingRequest.getDocument()).ifPresent(document -> {
 			newRequest.setName(document.getDescriptiveName());
 			newRequest.setFileName(document.getFileName());
 			newRequest.setRegistrationNumber(document.getRegistrationNumber());
 		});
-		Optional.ofNullable(signingRequest.getInitiator()).ifPresent(initiator -> newRequest.setInitiator(toInitiator(initiator)));
-		Optional.ofNullable(signingRequest.getNotificationMessage()).ifPresent(message -> newRequest.setNotificationMessage(toMessage(message)));
-		Optional.ofNullable(signingRequest.getReminder()).ifPresent(reminder -> newRequest.setReminder(toReminder(reminder)));
-		Optional.ofNullable(signingRequest.getSignatories()).ifPresent(signatories -> newRequest.setSignatories(toSignatories(signatories)));
-
 		return newRequest;
 	}
 
@@ -48,14 +46,12 @@ public final class EsigningMapper {
 		}
 		final var newSignatory = new generated.se.sundsvall.pw_e_signing.Signatory();
 
-		Optional.ofNullable(signatory.getOrganization()).ifPresent(newSignatory::setOrganization);
-		Optional.ofNullable(signatory.getPartyId()).ifPresent(newSignatory::setPartyId);
-		Optional.ofNullable(signatory.getName()).ifPresent(newSignatory::setName);
-		Optional.ofNullable(signatory.getEmail()).ifPresent(newSignatory::setEmail);
-		Optional.ofNullable(signatory.getNotificationMessage()).ifPresent(message -> newSignatory.setNotificationMessage(toMessage(message)));
-
+		newSignatory.setOrganization(signatory.getOrganization());
+		newSignatory.setPartyId(signatory.getPartyId());
+		newSignatory.setName(signatory.getName());
+		newSignatory.setEmail(signatory.getEmail());
+		newSignatory.setNotificationMessage(toMessage(signatory.getNotificationMessage()));
 		return newSignatory;
-
 	}
 
 	public static Set<generated.se.sundsvall.pw_e_signing.Signatory> toSignatories(final Set<Signatory> signatories) {
@@ -73,11 +69,10 @@ public final class EsigningMapper {
 		}
 		final var newInitiator = new generated.se.sundsvall.pw_e_signing.Initiator();
 
-		Optional.ofNullable(initiator.getOrganization()).ifPresent(newInitiator::setOrganization);
-		Optional.ofNullable(initiator.getPartyId()).ifPresent(newInitiator::setPartyId);
-		Optional.ofNullable(initiator.getName()).ifPresent(newInitiator::setName);
-		Optional.ofNullable(initiator.getEmail()).ifPresent(newInitiator::setEmail);
-
+		newInitiator.setOrganization(initiator.getOrganization());
+		newInitiator.setPartyId(initiator.getPartyId());
+		newInitiator.setName(initiator.getName());
+		newInitiator.setEmail(initiator.getEmail());
 		return newInitiator;
 	}
 
@@ -87,9 +82,8 @@ public final class EsigningMapper {
 		}
 		final var newMessage = new generated.se.sundsvall.pw_e_signing.Message();
 
-		Optional.ofNullable(message.getSubject()).ifPresent(newMessage::setSubject);
-		Optional.ofNullable(message.getBody()).ifPresent(newMessage::setBody);
-
+		newMessage.setBody(message.getBody());
+		newMessage.setSubject(message.getSubject());
 		return newMessage;
 	}
 
@@ -97,13 +91,11 @@ public final class EsigningMapper {
 		if (reminder == null) {
 			return null;
 		}
-
 		final var newReminder = new generated.se.sundsvall.pw_e_signing.Reminder();
 
-		Optional.of(reminder.getIntervalInHours()).ifPresent(newReminder::setIntervalInHours);
-		Optional.ofNullable(reminder.getStartDateTime()).ifPresent(newReminder::setStartDateTime);
-		Optional.ofNullable(reminder.getMessage()).ifPresent(message -> newReminder.setReminderMessage(toMessage(message)));
-
+		newReminder.setIntervalInHours(reminder.getIntervalInHours());
+		newReminder.setStartDateTime(reminder.getStartDateTime());
+		newReminder.setReminderMessage(toMessage(reminder.getMessage()));
 		return newReminder;
 	}
 
